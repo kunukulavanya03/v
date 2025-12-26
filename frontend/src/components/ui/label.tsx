cc81549ceec7ff34d422a7a7d59e76a8e4e317c4
kunukulavanya03 <kunukulavanya03@gmail.com>
2025-12-26 16:55:26 +0530

    🚀 Initial commit - Generated with CODE AGENT

--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -1,7 +1,10 @@
 "use client";
+
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label@2.1.2";
+
 import { cn } from "./utils";
+
 function Label({
   className,
   ...props
@@ -17,4 +20,5 @@ function Label({
     />
   );
 }
+
 export { Label };
